778c836c03ed4bbcd435c89768c9e104be3f01a4
Adnan Kibe <adnangitonga@gmail.com>
2020-02-14 17:55:26 +0300

    Addd a function to the submit button.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,56 @@ document.getElementById("marker").addEventListener("click", function(event){
 });
 function callingName(){
   var dayTime = document.getElementById("dateValue").valueAsDate;
-  var date = dayTime.getDate();
-  var month = dayTime.getMonth() + 1;
-  var year = dayTime.getFullYear();
-  var dateArray= [date];
-  console.log(dateArray);
+  var date = parseInt(dayTime.getDate());
+  var month = parseInt(dayTime.getMonth() + 1);
+  var year = parseInt(dayTime.getFullYear());
+  var male = document.getElementById("male").value;
+  var female = document.getElementById("female").value;
+  var malenames = ["Kwasi","Kwadwo","Kwadena","Kwaku","Yaw","Kofi","Kwame"]
+  var femalenames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+  var birthD = new Date(date +'/'+month+'/'+year);
+    var myDay = birthD.getDay()
+
+    if (myDay == 0)
+    {
+        Day = "Sunday";
+    }
+    else if (myDay == 1)
+    {
+        Day = "Monday";
+    }
+    else if (myDay == 2)
+    {
+        Day = "Tuesday";
+    }
+    else if (myDay == 3)
+    {
+        Day = "Wednesday";
+    }
+    else if (myDay == 4)
+    {
+        Day = "Thursday";
+    }
+    else if (myDay == 5)
+    {
+        Day = "Friday";
+    }
+    else
+    {
+        Day = "Saturday";
+    }
+
+    if (document.getElementById('male').checked) {
+      document.getElementById("akanName").innerText = "Your Akan name is "+malenames[myDay]+" because you were born on " +Day
+
+    }
+    else if (document.getElementById('female').checked){
+      document.getElementById("akanName").innerText = "Your Akan name is "+ femalenames[myDay]+" because you were born on " +Day
+
+    }
+    else{
+      document.getElementById("akanName").innerHTML = "I cannot get your Akan name";
+    }
 
 }
