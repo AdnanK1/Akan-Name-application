0632c080c587f69746d0faa20d123c086d7193a1
Adnan Kibe <adnangitonga@gmail.com>
2020-02-15 22:17:01 +0300

    Made changes to the form.

--- js/script.js
+++ js/script.js
@@ -2,57 +2,66 @@ document.getElementById("marker").addEventListener("click", function(event){
   event.preventDefault()
 });
 function callingName(){
-  var dayTime = document.getElementById("dateValue").valueAsDate;
-  var date = parseInt(dayTime.getDate());
-  var month = parseInt(dayTime.getMonth() + 1);
-  var year = parseInt(dayTime.getFullYear());
+  var date = document.getElementById("DD").value;
+  var year = document.getElementById("YY").value;
+  var month = document.getElementById("MM").value;
   var male = document.getElementById("male").value;
   var female = document.getElementById("female").value;
   var malenames = ["Kwasi","Kwadwo","Kwadena","Kwaku","Yaw","Kofi","Kwame"]
   var femalenames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
-  var birthD = new Date(date +'/'+month+'/'+year);
-    var myDay = birthD.getDay()
+  //using an inbuilt function
+  var birth = new Date(month+'/'+date'/'+year);
+  var myDay = birth.getDay()
 
-    if (myDay == 1)
-    {
-        Day = "Sunday";
-    }
-    else if (myDay == 2)
-    {
-        Day = "Monday";
-    }
-    else if (myDay == 3)
-    {
-        Day = "Tuesday";
-    }
-    else if (myDay == 4)
-    {
-        Day = "Wednesday";
-    }
-    else if (myDay == 5)
-    {
-        Day = "Thursday";
-    }
-    else if (myDay == 6)
-    {
-        Day = "Friday";
-    }
-    else
-    {
-        Day = "Saturday";
-    }
+  if (myDay == 0)
+  {
+      Day = "Sunday";
+  }
+  else if (myDay == 1)
+  {
+      Day = "Monday";
+  }
+  else if (myDay == 2)
+  {
+      Day = "Tuesday";
+  }
+  else if (myDay == 3)
+  {
+      Day = "Wednesday";
+  }
+  else if (myDay == 4)
+  {
+      Day = "Thursday";
+  }
+  else if (myDay == 5)
+  {
+      Day = "Friday";
+  }
+  else
+  {
+      Day = "Saturday";
+  }
 
-    if (document.getElementById('male').checked) {
-      document.getElementById("akanName").innerText = "Your Akan name is "+malenames[myDay]+" because you were born on " +Day
+  if(DD<0 || DD>31){
+    alert("Please Enter a valid day")
+  }
+  if(MM<0 || MM>12){
+    alert("Please Enter a valid Month")
+  }
+  if(YY)
 
-    }
-    else if (document.getElementById('female').checked){
-      document.getElementById("akanName").innerText = "Your Akan name is "+ femalenames[myDay]+" because you were born on " +Day
+  if (document.getElementById('male').checked) {
+    document.getElementById("akanName").innerText = "Your Akan name is "+malenames[myDay]+" because you were born on " +Day
 
-    }
-    else{
-      document.getElementById("akanName").innerHTML = "I cannot get your Akan name";
-    }
+  }
+  else if (document.getElementById('female').checked){
+    document.getElementById("akanName").innerText = "Your Akan name is "+ femalenames[myDay]+" because you were born on " +Day
+
+  }
+  else{
+    document.getElementById("akanName").innerHTML = "I cannot get your Akan name";
+  }
+
 
 }
